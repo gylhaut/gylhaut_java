f22385de4aaab43d0290b7e9b7f9a63128abb01d
gaoyunling <gaoyunling@qk365.com>
2019-11-27 11:24:47 +0800

    过滤idea

---
+++ java_design_patterns/src/main/java/Display.java
@@ -0,0 +1,11 @@
+public abstract class Display {
+    public abstract int getColunms();
+    public abstract int getRows();
+    public abstract String getTowText(int rowID);
+    public void show(){
+        for (int i = 0; i < getRows(); i++) {
+            System.out.println(getTowText(i));
+        }
+    }
+
+}
